5c71105de45e04746efbc4c95d10b591c4f57cb5
sheepeatsbear <pvoskou@gmail.com>
2021-10-17 08:47:33 +0100

    added common testing. updated app component spec file

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,12 +1,26 @@
 import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
+import { MediaObserver } from '@angular/flex-layout';
+import { MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {
+  commonTestingModules,
+  commonTestingProviders,
+  DomSanitizerFake,
+  MatIconRegistryFake,
+  MediaObserverFake,
+} from './common/common.testing';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [RouterTestingModule],
+      imports: commonTestingModules,
+      providers: commonTestingProviders.concat([
+        { provide: MediaObserver, useClass: MediaObserverFake },
+        { provide: MatIconRegistry, useClass: MatIconRegistryFake },
+        { provide: DomSanitizer, useClass: DomSanitizerFake },
+      ]),
       declarations: [AppComponent],
     }).compileComponents();
   });
