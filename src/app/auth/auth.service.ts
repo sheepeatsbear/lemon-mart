1bd135704ac3a4a1d6af22295f533cf9f167d5be
sheepeatsbear <pvoskou@gmail.com>
2021-10-18 12:01:49 +0100

    Added auth implemenation and inmemory service

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,6 +1,91 @@
 import { Injectable } from '@angular/core';
+import jwtDecode from 'jwt-decode';
+import { BehaviorSubject, Observable, throwError } from 'rxjs';
+import { catchError, filter, map, mergeMap, tap } from 'rxjs/operators';
 
+import { transformError } from '../common/common';
+import { IUser, User } from '../user/user/user';
+import { Role } from './auth';
+import { CacheService } from './cache.service';
+
+export interface IAuthStatus {
+  isAuthenticated: boolean;
+  userRole: Role;
+  userId: string;
+}
+
+export interface IServerAuthResponse {
+  accessToken: string;
+}
+
+export const defaultAuthStatus: IAuthStatus = {
+  isAuthenticated: false,
+  userRole: Role.None,
+  userId: '',
+};
+
+export interface IAuthService {
+  readonly authStatus$: BehaviorSubject<IAuthStatus>;
+  readonly currentUser$: BehaviorSubject<IUser>;
+  login(email: string, password: string): Observable<void>;
+  logout(clearToken?: boolean): void;
+  getToken(): string;
+}
 @Injectable()
-export class AuthService {
-  constructor() {}
+export abstract class AuthService extends CacheService implements IAuthService {
+  constructor() {
+    super();
+  }
+
+  readonly authStatus$ = new BehaviorSubject<IAuthStatus>(defaultAuthStatus);
+  readonly currentUser$ = new BehaviorSubject<IUser>(new User());
+
+  login(email: string, password: string): Observable<void> {
+    const loginResponse$ = this.authProvider(email, password).pipe(
+      map((value) => {
+        this.setToken(value.accessToken);
+        const token = jwtDecode(value.accessToken);
+        return this.transformJwtToken(token);
+      }),
+
+      tap((status) => this.authStatus$.next(status)),
+      filter((status: IAuthStatus) => status.isAuthenticated),
+      mergeMap(() => this.getCurrentUser()),
+      map((user) => this.currentUser$.next(user)),
+      catchError(transformError)
+    );
+    loginResponse$.subscribe({
+      error: (err) => {
+        this.logout();
+        return throwError(err);
+      },
+    });
+    return loginResponse$;
+  }
+
+  logout(clearToken?: boolean): void {
+    if (clearToken) {
+      this.clearToken();
+    }
+    setTimeout(() => this.authStatus$.next(defaultAuthStatus), 0);
+  }
+
+  protected setToken(jwt: string) {
+    this.setItem('jwt', jwt);
+  }
+
+  getToken(): string {
+    return this.getItem('jwt') ?? '';
+  }
+
+  protected clearToken() {
+    this.removeItem('jwt');
+  }
+
+  protected abstract authProvider(
+    email: string,
+    password: string
+  ): Observable<IServerAuthResponse>;
+  protected abstract transformJwtToken(token: unknown): IAuthStatus;
+  protected abstract getCurrentUser(): Observable<User>;
 }
