499d8fc49d1dd82ad3be0fccc5a8f9234756c282
sheepeatsbear <pvoskou@gmail.com>
2021-10-19 12:51:15 +0100

    ran style fix

--- src/app/auth/auth-guard.service.spec.ts
+++ src/app/auth/auth-guard.service.spec.ts
@@ -1,6 +1,6 @@
 import { TestBed } from '@angular/core/testing';
-import { commonTestingModules, commonTestingProviders } from '../common/common.testing';
 
+import { commonTestingModules, commonTestingProviders } from '../common/common.testing';
 import { AuthGuard } from './auth-guard.service';
 
 describe('AuthGuardService', () => {
